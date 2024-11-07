94909cdb0a71aa05680f081aef3423a912eb1ea8
dispear <kkm7hjh@naver.com>
2024-11-07 10:46:09 +0900

    commit

--- src/main/java/org/example/jenkins/controller/CalculatorController.java
+++ src/main/java/org/example/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "argocd sync test123";
+        return "argocd sync test";
     }
 
     @GetMapping("/plus")
